39ce3dae2cc9927855ba45f6b0dccae4a309198a
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-12-16 10:52:19 -0500

    add random quotes

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const app=require('express')()
 const port=3000
 
+/**
+ * Returns a random item from the list that you supply.
+ * 
+ * @param {Array<T>} list The list that you wish to get the random item from.
+ * @returns {T} Random item in list
+ */
+function randomItemFrom(list) {
+    return list[Math.floor(Math.random() * list.length)];
+}
+
+
+/**
+ * Gets a list of rumi quotes.
+ *
+ * @returns {Array<string>}
+ */
+function rumiQuotes() {
+    return require('../rumi').Quotes;
+}
+
+
 app.get('/',(req,res)=>{
 	console.log('GET /')
 	res.send('Hi from Rumi')
 })
 
 app.get('/quote',(req,res)=>{
-	console.log('GET /quote')
-	res.send('Insert random rumi quote here somehow....')
+	console.log('GET /quote');
+	res.send(randomItemFrom(rumiQuotes()));
 })
 
 app.listen(port,()=>console.log(`git-a-rumi app is listening on port ${port}`))
